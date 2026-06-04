c1e40c4ac45bc9e7a97f47666717f230e2d3d5b9
Taher Karu <Taherkaru53@gmail.com>
2026-06-04 18:18:27 +0530

    Integrated Redux, initialized Tailwind, and created components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
-import './App.css'
-function App() {
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { login, logout } from "./Storage/AuthSlice";
+import Header from "./components/Header/Header.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import service from "./AppWrite/Auth";
 
-  return (
-    <>
-    <h1>Taher first react App</h1>
-    </>
-  )
+
+function App() {
+  const [loading, setloading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    service
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(setloading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          {/* outlet */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
-export default App
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      mello
+    </div>
+  )
+}
+
+export default Header
